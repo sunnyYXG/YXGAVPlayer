26b9b22b7192387e8ecde55f4c973d80de5b80fa
sunnyYXG <yuan890127@126.com>
2017-06-18 09:24:57 +0800

    创建
    工程

---
+++ YXGAVPlayer/YXGAVPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YXGAVPlayer
+//
+//  Created by sunny_FX on 2017/6/18.
+//  Copyright © 2017年 YXG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YXGAVPlayer/YXGAVPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YXGAVPlayer
+//
+//  Created by sunny_FX on 2017/6/18.
+//  Copyright © 2017年 YXG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
